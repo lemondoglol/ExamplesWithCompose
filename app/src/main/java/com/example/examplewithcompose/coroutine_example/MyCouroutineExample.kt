33139f95fca83e198a5b614b31fe4025470f90ad
lemondoglol <lemondoglol@github.com>
2022-09-14 18:15:33 -0400

    bug fix

--- app/src/main/java/com/example/examplewithcompose/coroutine_example/MyCouroutineExample.kt
+++ app/src/main/java/com/example/examplewithcompose/coroutine_example/MyCouroutineExample.kt
@@ -1,8 +1,7 @@
 package com.example.examplewithcompose.coroutine_example
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import android.util.Log
+import kotlinx.coroutines.*
 
 /**
  * best practice if you want to make your coroutine run in sequence
@@ -55,4 +54,57 @@ private fun funLaunchAnotherScope() {
 private fun funLaunchAnotherScopeContent() {
     // do something here
     normalFun()
+}
+
+/**
+ * following is for .launch and .async
+ * */
+val supervisorJob = SupervisorJob()
+val myScope = CoroutineScope(Dispatchers.IO + supervisorJob + CoroutineName("myOwnScope"))
+
+fun launchVSAsync() {
+    myScope.launch {
+//        Log.d("Tuna", this.coroutineContext.toString())
+        doSomeV1()
+        Log.d("Tuna", "+++++++++")
+        doSomeV2()
+    }
+}
+
+private suspend fun doSomeV1() = withContext(Dispatchers.IO) {
+    val jobLst = mutableListOf<Deferred<Int>>()
+    for (index in 0..5) { // no bug here
+        jobLst.add(
+            async {
+//                Log.d("Tuna", this.coroutineContext.toString())
+                mockAPICall(index)
+            }
+        )
+    }
+    jobLst.awaitAll().onEach {
+        Log.d("Tuna", it.toString())
+    }
+}
+
+private suspend fun doSomeV2() = withContext(Dispatchers.IO) {
+    val jobLst = mutableListOf<Deferred<Int>>()
+    var index = 0
+    while (index <= 5) { // this is causing the issue; for loop is okay
+        val newIndex = index
+        jobLst.add(
+            async {
+                mockAPICall(newIndex)
+            }
+        )
+        index += 1
+    }
+
+    jobLst.awaitAll().onEach {
+        Log.d("Tuna", it.toString())
+    }
+}
+
+private suspend fun mockAPICall(id: Int): Int {
+    delay(1000L)
+    return id
 }
